7afa8b500d4e09625e9e6ca9e51aba34d11c6227
achrafmagouh <53151871+achrafmagouh@users.noreply.github.com>
2019-08-30 15:22:41 -0600

    Update derbyTests.js

--- tests/derbyTests.js
+++ tests/derbyTests.js
@@ -15,35 +15,4 @@ module.exports = {
     'Derby Visitor Info': browser => {
         test(derby)
     },
-
-
-        
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            // .moveToElement(locator1, XOffset1, Yoffset1)
-        // .pause(100) // if you want to give wait
-        // .mouseButtonDown(0) // press click, down
-        // .moveToElement(locator2, XOffset2, Yoffset2) // move to element
-        // .mouseButtonUp(0); // press release , up
+}
